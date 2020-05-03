33c7d2da806eb1d110164938d53cdf1509130b02
Maryanto Cin <firevolt719@gmail.com>
2020-05-03 23:26:58 +0700

    added auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Test
+                    Testing
                 </div>
 
                 <div class="links">
